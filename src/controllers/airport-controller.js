9c359fab8c02e8dfd84ef08e8f92184b198458e9
snkt-30 <sanketkhonde30@gmail.com>
2024-05-19 18:07:17 +0530

    Added error code enums

--- src/controllers/airport-controller.js
+++ src/controllers/airport-controller.js
@@ -1,11 +1,12 @@
 const { AirportService } = require("../services/index");
+const { SuccessCodes } = require("../utils/error-codes");
 
 const airportService = new AirportService();
 
 const create = async (req, res) => {
   try {
     const response = await airportService.create(req.body);
-    return res.status(200).json({
+    return res.status(SuccessCodes.OK).json({
       data: response,
       success: true,
       message: "Airport has been created successfully",
---
+++ src/utils/error-codes.js
@@ -0,0 +1,22 @@
+const ClientError = Object.freeze({
+  BAD_REQUEST: 400,
+  UNAUTHORISED: 401,
+  NOT_FOUND: 404,
+});
+
+const ServerError = Object.freeze({
+  INTERNAL_SERVER_ERROR: 500,
+  NOT_IMPLEMENTED: 501,
+});
+
+const SuccessCodes = Object.freeze({
+  OK: 200,
+  CREATED: 201,
+});
+
+
+module.exports={
+    ClientError,
+    ServerError,
+    SuccessCodes
+}
